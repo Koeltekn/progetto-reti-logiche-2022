c1c38fadaccf392e29b860ff9a80ffbb6c02d7b6
Koeltekn <barletta.andrea@outlook.com>
2022-06-01 15:02:52 +0200

    Convoluter convolutes, serializer serializes, deserializes bugs

--- 10665046_10567531.vhd
+++ 10665046_10567531.vhd
@@ -1,18 +1,33 @@
 -- Number of words to be read saved at address 0
 -- First byte of the input sequence saved at address 1
 -- First byte of the output sequence has to be saved from byte 1000 (decimal)
-
+-----------------------------------------------------------------------------------------
 -- First signal to be received is Reset
 -- Before each elaboration, the Start signal must be set to HIGH, and kept HIGH untill the Done signal is set HIGH
 -- The Done signal is set HIGH after the last byte has been saved to memory 
 -- The Done signal must be kept HIGH untill the Start signal is set LOW
 -- A new Start signal cannot be sent until DONE is set LOW
 -- When the Start signal is set HIGH, the convoluter must be reset (state 00)
+-----------------------------------------------------------------------------------------
+-- Memory writing is asynchronous, memory reading is synchronous (the data gets read after 1 clock cycle)
+-----------------------------------------------------------------------------------------
+-- To read from memory
+-- o_address set to the memory address to be read
+-- o_en set to high
+-- o_we set to low
+-- read data is in i_data (AFTER 1 CLOCK CYCLE)
+-----------------------------------------------------------------------------------------
+-- To write to memory
+-- o_address set to the memory address to be written into
+-- o_en set to high
+-- o_we set to high
+-- o_data set to the data to be written
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity project_reti_logiche is
     port (
@@ -24,88 +39,169 @@ entity project_reti_logiche is
         o_done : out std_logic;                         --End of computation
         o_en : out std_logic;                           --Enable input and output to memory
         o_we : out std_logic;                           --1 to write to memory, 0 to read
-        o_data : out std_logic_vector (7 downto 0);     --Data to be written to memory (after being deserialized)
-        -- TEST BENCH SIGNALS
-        counter_tb : inout integer
+        o_data : out std_logic_vector (7 downto 0)      --Data to be written to memory (after being deserialized)
     );
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
-    constant zero: std_logic := '0';
+    -- FSA STATE
+    type S is (IDLE, START, READING_NUMBER_OF_BYTES, NUMBER_OF_BYTES_READ, READING_BYTE, BYTE_READ,START_SERIALIZE, SERIALIZATION_ENDED);
+    signal state : S;
+    -- INPUT DATA
+    signal number_of_bytes: integer;
+    signal input_memory_address: std_logic_vector(15 downto 0);
+    signal input_byte: std_logic_vector(0 to 7);
+    -- OUTPUT DATA
+    signal output_memory_address: std_logic_vector(15 downto 0);
+    -- SERIALIZATION
+    signal serialize_enable: std_logic;
     signal serialized_bit: std_logic;
-    signal counter: integer;
-    signal serialization_done: std_logic:='0';
-    type S is (S0,S1,S2,S3,S4,S5,S6);
-    signal cur_state, next_state : S;
+    signal serialization_done: std_logic;
+    -- CONVOLUTER
+    signal p1:std_logic;
+    signal p2:std_logic;
+    -- DESERIALIZATION
+    signal deserialize_enable: std_logic;
+    signal deserialize_done:std_logic;
+    -- DEBUG SIGNALS
+    signal counter_deb:integer;
+    signal d1_deb:std_logic;
+    signal d2_deb:std_logic;
+    signal output_byte_db:std_logic_vector(7 downto 0);
 begin
-    process(i_clk, i_rst, i_start)
-    begin
-        if(i_rst = '1') then
-            cur_state <= S0;
-        elsif rising_edge(i_clk) then
-            cur_state <= next_state;
-        end if;
-    end process;
-
-    process(cur_state, i_start)
-    begin
-        next_state <= cur_state;
-        -- case cur_state is
-            -- when S0 =>
-            --     if i_start = '1' then
-            --         next_state <= S1;
-            --     end if;
-            -- when S1 =>
-            --     next_state <= S2;
-            -- when S2 =>
-            --     if o_end = '1' then
-            --         next_state <= S4;
-            --     else
-            --         next_state <= S3;
-            --     end if;
-            -- when S3 =>
-            --     if o_end = '1' then
-            --         next_state <= S4;
-            --     else
-            --         next_state <= S3;
-            --     end if;
-            -- when S4 =>
-            --     next_state <= S5;
-            -- when S5 =>
-            --     next_state <= S6;
-            -- when S6 =>
-            --     next_state <= S0;
-        -- end case;
-    end process;
-    
-    process(cur_state)
-    begin
-        -- case cur_state is
-            -- when S0 =>
-            -- when S1 =>
-            -- when S2 =>
-            -- when S3 =>
-            -- when S4 =>
-            -- when S5 =>
-            -- when S6 =>
-        -- end case;
-    end process;
-
-    serializer: process(i_clk,i_rst)
+    next_state_function: process(i_clk, i_rst)
     begin
         if i_rst='1' then
-            counter<=0;
-            counter_tb<=0;
-            serialization_done<='0';
+            state<=IDLE;
         elsif rising_edge(i_clk) then
-            counter<=counter+1;
-            counter_tb<=counter_tb+1;
-            serialized_bit<=i_data(counter);
-            if counter=7 then
-                counter<=0;
-                counter_tb<=0;
-                serialization_done<='1';
-            end if;
+            case state is
+                when IDLE =>
+                    if i_start='1' then
+                        state<=START;
+                    end if;
+                when START =>
+                    state<=READING_NUMBER_OF_BYTES;
+                when READING_NUMBER_OF_BYTES =>
+                    state<=NUMBER_OF_BYTES_READ;
+                when NUMBER_OF_BYTES_READ =>
+                    state<=READING_BYTE;
+                when READING_BYTE=>
+                    state<=BYTE_READ;
+                when BYTE_READ=>
+                    state<=START_SERIALIZE;
+                when START_SERIALIZE=>
+                    if serialization_done='1' then
+                        state<=SERIALIZATION_ENDED;
+                    end if;
+                when SERIALIZATION_ENDED=>
+            end case;
         end if;
+    end process next_state_function;
+
+    output_state_function: process(state)
+    begin
+        case state is
+            when IDLE =>
+            when START =>
+                -- Reset signals for new elaboration
+                serialize_enable<='0';
+                number_of_bytes<=0;
+                input_memory_address<=std_logic_vector(to_unsigned(1,16));
+                output_memory_address<=std_logic_vector(to_unsigned(1000,16));
+                input_byte<=std_logic_vector(to_unsigned(0,8));
+                -- Read number of bytes from memory
+                o_address<=std_logic_vector(to_unsigned(0,16));
+                o_en<='1';
+                o_we<='0';
+            when READING_NUMBER_OF_BYTES =>
+            when NUMBER_OF_BYTES_READ =>
+                -- Save the number of bytes to be converted
+                number_of_bytes<=to_integer(unsigned(i_data));
+                -- Read the first byte
+                o_address<=input_memory_address;
+            when READING_BYTE=>
+            when BYTE_READ=>
+                o_en<='0';
+                input_byte<=i_data;
+            when START_SERIALIZE=>
+                serialize_enable<='1';
+            when SERIALIZATION_ENDED=>
+                serialize_enable<='0';
+        end case;
+    end process output_state_function;
+
+    serializer: process(i_clk,i_start)
+        variable counter:integer;
+    begin
+        if serialize_enable='1' then
+            if falling_edge(i_clk) then
+                if counter=4 then
+                    serialization_done<='1';
+                    o_done<='1';
+                else
+                    serialized_bit<=input_byte(counter);
+                    counter:=counter+1;
+                end if;   
+            end if;
+        elsif i_start='1' then
+            counter:=0;
+            serialization_done<='0';
+            serialized_bit<='U';
+        end if;
+        counter_deb<=counter;
     end process serializer;
+
+    convoluter: process(i_clk,i_start)
+        variable d1:std_logic;
+        variable d2:std_logic;       
+    begin
+        if serialize_enable='1' then
+            if rising_edge(i_clk) then
+                p1<=serialized_bit xor d2;
+                p2<=serialized_bit xor d1 xor d2;
+                d2:=d1;
+                d1:=serialized_bit;
+                deserialize_enable<='1';
+            end if;
+        elsif i_start='1' then
+            d1:='0';
+            d2:='0';
+            p1<='0';
+            p2<='0';
+            deserialize_enable<='0';
+        end if;
+        d1_deb<=d1;
+        d2_deb<=d2;
+    end process convoluter;
+
+    deserializer: process(i_clk,i_start)
+        variable counter1:integer;
+        variable counter2:integer;
+        variable output_byte: std_logic_vector(7 downto 0);
+    begin
+        if deserialize_done='1' then
+            -- Write to memory
+            --o_address<=std_logic_vector(to_unsigned(1000,16));
+            --o_en<='1';
+            o_we<='1';
+            --o_data<=output_byte;
+        elsif deserialize_enable='1' then
+            if falling_edge(i_clk) then
+                if counter1=8 then
+                    deserialize_done<='1';
+                else
+                    output_byte(counter1):=p1;
+                    output_byte(counter2):=p2;
+                    counter1:=counter1+2;
+                    counter2:=counter2+2;
+                end if;
+            end if;
+        elsif i_start='1' then
+            counter1:=0;
+            counter2:=1;
+            deserialize_done<='0';
+            output_byte:=std_logic_vector(to_unsigned(0,8));
+        end if;
+        output_byte_db<=output_byte;
+    end process deserializer;
 end Behavioral;
--- test_bench.vhd
+++ test_bench.vhd
@@ -1,47 +1,97 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
 
 entity project_tb is
-end project_tb;
-    
-architecture tb of project_tb is
-    signal i_start, i_clk, i_rst : std_logic;  -- inputs 
-    signal i_data : std_logic_vector(7 downto 0); 
-    signal counter_tb: integer;
-begin
-    -- connecting testbench signals with half_adder.vhd
-    UUT : entity work.project_reti_logiche port map (i_clk=>i_clk,i_rst=>i_rst, i_start=>i_start, i_data=>i_data, counter_tb=>counter_tb);
+    end project_tb;
 
-    -- inputs
-    -- 00 at 0 ns
-    -- 01 at 20 ns, as b is 0 at 20 ns and a is changed to 1 at 20 ns
-    -- 10 at 40 ns
-    -- 11 at 60 ns
-    i_clk <= '0', 
-        '1' after 10 ns,
-        '0' after 20 ns,
-        '1' after 30 ns,
-        '0' after 40 ns,
-        '1' after 50 ns,
-        '0' after 60 ns,
-        '1' after 70 ns,
-        '0' after 80 ns,
-        '1' after 90 ns,
-        '0' after 100 ns,
-        '1' after 110 ns,
-        '0' after 120 ns,
-        '1' after 130 ns,
-        '0' after 140 ns,
-        '1' after 150 ns,
-        '0' after 160 ns,
-        '1' after 170 ns,
-        '0' after 180 ns,
-        '1' after 190 ns,
-        '0' after 200 ns,
-        '1' after 210 ns,
-        '0' after 220 ns,
-        '1' after 230 ns,
-        '0' after 340 ns;
-    i_rst <= '1', '0' after 5 ns;
-end tb ;
+architecture projecttb of project_tb is
+    constant c_CLOCK_PERIOD         : time := 100 ns;
+    signal   tb_done                : std_logic;
+    signal   mem_address            : std_logic_vector (15 downto 0) := (others => '0');
+    signal   tb_rst                 : std_logic := '0';
+    signal   tb_start               : std_logic := '0';
+    signal   tb_clk                 : std_logic := '0';
+    signal   mem_o_data,mem_i_data  : std_logic_vector (7 downto 0);
+    signal   enable_wire            : std_logic;
+    signal   mem_we                 : std_logic;
+
+
+type ram_type is array (65535 downto 0) of std_logic_vector(7 downto 0);
+
+
+signal RAM: ram_type := (0 => std_logic_vector(to_unsigned(2  , 8)), 
+                            1 => std_logic_vector(to_unsigned(  162  , 8)), 
+                            2 => std_logic_vector(to_unsigned(  75  , 8)),  
+                            others => (others =>'0'));
+                            
+                            
+component project_reti_logiche is
+    port (
+            i_clk         : in  std_logic;
+            i_start       : in  std_logic;
+            i_rst         : in  std_logic;
+            i_data        : in  std_logic_vector(7 downto 0);
+            o_address     : out std_logic_vector(15 downto 0);
+            o_done        : out std_logic;
+            o_en          : out std_logic;
+            o_we          : out std_logic;
+            o_data        : out std_logic_vector (7 downto 0)
+    );
+end component project_reti_logiche;
+
+begin
+    UUT: project_reti_logiche
+        port map (
+                i_clk      	=> tb_clk,
+                i_start     => tb_start,
+                i_rst      	=> tb_rst,
+                i_data    	=> mem_o_data,
+                o_address  	=> mem_address,
+                o_done      => tb_done,
+                o_en    	=> enable_wire,
+                o_we 		=> mem_we,
+                o_data    	=> mem_i_data
+        );
+    
+    p_CLK_GEN : process is
+    begin
+        wait for c_CLOCK_PERIOD/2;
+        tb_clk <= not tb_clk;
+    end process p_CLK_GEN;
+    
+    
+    MEM : process(tb_clk)
+    begin
+        if tb_clk'event and tb_clk = '1' then
+            if enable_wire = '1' then
+                if mem_we = '1' then
+                    RAM(conv_integer(mem_address))  <= mem_i_data;
+                    mem_o_data                      <= mem_i_data after 1 ns;
+                else
+                    mem_o_data <= RAM(conv_integer(mem_address)) after 1 ns;
+                end if;
+            end if;
+        end if;
+    end process;
+    
+
+    test : process is
+    begin 
+        tb_start <= '0';
+        wait for 100 ns;
+        wait for c_CLOCK_PERIOD;
+        tb_rst <= '1';
+        wait for c_CLOCK_PERIOD;
+        wait for 100 ns;
+        tb_rst <= '0';
+        wait for c_CLOCK_PERIOD;
+        wait for 100 ns;
+        tb_start <= '1';
+        wait for c_CLOCK_PERIOD;
+        wait for 2000 ns;
+        assert false report "Simulation Ended! TEST PASSATO (ENCODE_EXAMPLE)" severity failure;
+    end process test;
+
+end projecttb; 
